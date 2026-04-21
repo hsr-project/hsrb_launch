87bf6cd034b8635a2fcadb49954e8037170aaa7e
Keisuke Takeshita <keisuke.tksht@gmail.com>
2026-04-21 17:50:53 +0900

    Enable tmc_joint_command_controller::BasicJointCommandController in RViz simulator.

--- hsrb_rviz_simulator/launch/hsrb_rviz_simulator.launch.py
+++ hsrb_rviz_simulator/launch/hsrb_rviz_simulator.launch.py
@@ -1,34 +1,11 @@
 #!/usr/bin/env python3
-# Copyright (c) 2025 TOYOTA MOTOR CORPORATION
-# All rights reserved.
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted (subject to the limitations in the disclaimer
-# below) provided that the following conditions are met:
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * Neither the name of the copyright holder nor the names of its contributors may be used
-#   to endorse or promote products derived from this software without specific
-#   prior written permission.
-# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-# LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-# DAMAGE.
+# Copyright (C) 2022 Toyota Motor Corporation
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.action import Action
 from launch.actions import (
     DeclareLaunchArgument,
     ExecuteProcess,
@@ -85,6 +62,13 @@ def declare_arguments():
                                                     description='Use navigation if true'))
 
     return declared_arguments
+
+
+def create_limiter_and_controller(limiter_name: str, controller_name: str) -> list[Action]:
+    limiter_controller_spawner = create_spawner_node(limiter_name)
+    controller_spawner = create_spawner_node(controller_name)
+    return [limiter_controller_spawner,
+            set_on_process_exit_event_handler(limiter_controller_spawner.actions[0], controller_spawner.actions)]
 
 
 def generate_launch_description():
@@ -159,28 +143,30 @@ def generate_launch_description():
                                                        'maps', 'white_space', 'map.yaml'])}.items(),
         condition=IfCondition(LaunchConfiguration('use_navigation')))
 
-    # If the scan topic is not published, rosnav will not work.
+    # scanトピックが出ていない場合，rosnavだと動作しないので
     dummy_scan_publisher = ExecuteProcess(cmd=['ros2', 'topic', 'pub', '/scan', 'sensor_msgs/msg/LaserScan',
                                                '{header: {stamp: now, frame_id: base_range_sensor_link}}'],
                                           condition=IfCondition(LaunchConfiguration('use_navigation')))
 
-    motion_command_limitter_controller_spawner = create_spawner_node('motion_command_limitter_controller')
-    omni_base_controller_spawner = create_spawner_node('omni_base_controller')
+    head_trajectory_controller_spawner = create_limiter_and_controller(
+        'head_motion_command_limiter_controller', 'head_trajectory_controller')
+    arm_trajectory_controller_spawner = create_limiter_and_controller(
+        'arm_motion_command_limiter_controller', 'arm_trajectory_controller')
+    omni_base_controller_spawner = create_limiter_and_controller(
+        'base_motion_command_limiter_controller', 'omni_base_controller')
     nodes = [control_node,
              joint_state_publisher,
              robot_state_pub_node,
              wheel_odom_connector_tf,
              create_spawner_node('joint_state_broadcaster'),
-             create_spawner_node('head_trajectory_controller'),
-             create_spawner_node('arm_trajectory_controller'),
              create_spawner_node('gripper_controller'),
-             motion_command_limitter_controller_spawner,
-             set_on_process_exit_event_handler(motion_command_limitter_controller_spawner.actions[0],
-                                               omni_base_controller_spawner.actions),
              rviz_node,
              hsrb_teleop,
              hsrb_manipulation,
              hsrb_navigation,
              dummy_scan_publisher]
+    nodes += head_trajectory_controller_spawner
+    nodes += arm_trajectory_controller_spawner
+    nodes += omni_base_controller_spawner
 
     return LaunchDescription(declare_arguments() + nodes)
